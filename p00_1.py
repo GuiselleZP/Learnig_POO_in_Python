7e9d5f5472ea1043cb5587e9d1925c19e0ea3eb9
Guiselle Tatiana Zambrano Penagos <guiselle2097@gmail.com>
2019-10-24 06:08:00 -0500

    I created an encapsulated method and new attributes

--- p00_1.py
+++ p00_1.py
@@ -1,22 +1,40 @@
 class Car():
     # Initial state
     def __init__(self):
-        self.long_car = 250
-        self.width  = 120
-        # Encapsulated
+        self.__long_car = 250
+        self.__width  = 120
         self.__wheels = 4
         self.__moving = False
     
     def move(self, it_move):
-        self.moving = it_move
-        if(self.moving):
+        self.__moving = it_move
+        
+        if(self.__moving):
+            review = self.__internal_review()
+
+        if(self.__moving and review):
             return "The car is moving"
+        if(self.__moving and review == False):
+            return "Something has failed in the review, the car cannot start"
         else:
             return "The car is stopped"
 
     def state(self):
         print("The car has {} wheels, has a width of {}cm and a lenght of {}cm"\
-                .format(self.__wheels, self.width, self.long_car))
+                .format(self.__wheels, self.__width, self.__long_car))
+
+    def __internal_review(self):
+        print('Performing internal review')
+
+        self.oil = "ok"
+        self.gasoline = "failed"
+        self.doors = "closed"
+
+        if(self.gasoline == "ok" and self.oil == "ok" and self.doors == \
+            "closed"):
+            return True
+        else:
+            return False
             
 my_car = Car()
 print(my_car.move(True))
@@ -26,5 +44,3 @@ print('\nAnother car\n')
 
 my_car2 = Car()
 print(my_car2.move(False))
-my_car2.__wheels = 5
-my_car2.state()
